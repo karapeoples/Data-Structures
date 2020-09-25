eb1ae9161675dece2302203cf23b0034ab5c5f6e
karapeoples <kararpeoples@yahoo.com>
2020-09-25 13:25:01 -0500

    Only stretch on Module 1 (Queues)

--- q/q.py
+++ q/q.py
@@ -34,27 +34,27 @@ Stretch: What if you could only use instances of your Stack class to implement t
 
 #LinkedList
 
-# import sys
-# sys.path.append('singly_linked_list')
-# from singly_linked_list import LinkedList
+import sys
+sys.path.append('singly_linked_list')
+from singly_linked_list import LinkedList
 
-# class Queue:
-#     def __init__(self):
-#         self.size = 0
-#         self.storage = LinkedList()
+class Q:
+   def __init__(self):
+      self.size = 0
+      self.storage = LinkedList()
 
-#     def __len__(self):
-#         return self.size
+   def __len__(self):
+      return self.size
 
-#     def enqueue(self, value):
-#         self.storage.add_to_tail(value)
-#         self.size += 1
+   def enqueue(self, value):
+      self.storage.add_to_tail(value)
+      self.size += 1
 
-#     def dequeue(self):
-#         if self.size == 0:
-#             return None
-#         self.size -= 1
-#         return self.storage.remove_head()
+   def dequeue(self):
+      if self.size == 0:
+            return None
+      self.size -= 1
+      return self.storage.remove_head()
 
 
 #Stretch Implemented with Stack
